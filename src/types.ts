9cc6947281d718fcfb7d51740cb401079b1e72e8
rodrigodev <rodrigosousa1999rss@gmail.com>
2021-09-19 16:26:26 -0300

    m

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,14 @@
 export interface Props {
-    breakpoints: object;
-    row: boolean;
-    direction: 'row' | 'row-reverse' | 'column' | 'column-reverse' | 'inherit' | 'initial' | 'unset';
-    justify: 'center' | 'flex-start' | 'flex-end' | 'space-between' | 'space-around' | 'inherit' | 'initial' | 'unset';
-    align: 'center' | 'flex-start' | 'flex-end' | 'space-between' | 'space-around' | 'stretch' | 'inherit' | 'initial' | 'unset';
-    alignItems: 'normal' | 'stretch' | 'center' | 'start' | 'end' | 'flex-start' | 'flex-end' | 'self-start' | 'self-end' | 'baseline' | 'first baseline' | 'last baseline' | 'safe center' | 'unsafe center' | 'inherit' | 'initial' | 'unset';
-    spacing: number | object;
-    wrap: 'nowrap' | 'wrap' | 'wrap-reverse' | 'inherit' | 'initial' | 'unset';
-    self: 'auto' | 'center' | 'flex-start' | 'flex-end' | 'baseline' | 'stretch' | 'inherit' | 'initial' | 'unset';
-    order: number;
+    breakpoints?: object,
+    row?: boolean,
+    direction?: 'row' | 'row-reverse' | 'column' | 'column-reverse' | 'inherit' | 'initial' | 'unset',
+    justify?: 'center' | 'flex-start' | 'flex-end' | 'space-between' | 'space-around' | 'inherit' | 'initial' | 'unset',
+    align?: 'center' | 'flex-start' | 'flex-end' | 'space-between' | 'space-around' | 'stretch' | 'inherit' | 'initial' | 'unset',
+    alignItems?: 'normal' | 'stretch' | 'center' | 'start' | 'end' | 'flex-start' | 'flex-end' | 'self-start' | 'self-end' | 'baseline' | 'first baseline' | 'last baseline' | 'safe center' | 'unsafe center' | 'inherit' | 'initial' | 'unset',
+    spacing?: number | object,
+    wrap?: 'nowrap' | 'wrap' | 'wrap-reverse' | 'inherit' | 'initial' | 'unset';
+    self?: 'auto' | 'center' | 'flex-start' | 'flex-end' | 'baseline' | 'stretch' | 'inherit' | 'initial' | 'unset';
+    order?: number;
     xs?: number,
     'xs-m'?: number,
     sm?: number,
@@ -17,12 +17,14 @@ export interface Props {
     'md-m'?: number,
     lg?: number,
     'lg-m'?: number,
-    xl?: number
+    xl?: number,
+    children?: any
 }
 
 export type Spacing = Props['spacing'];
 export type mapObjectParams = [any, number]
 export interface PropsMedia extends Props {
-    _spacing: Props['spacing'];
-    _wrap: Props['wrap'];
+    _spacing?: Props['spacing'],
+    _wrap?: Props['wrap'],
+    breakpoints: object
 }
